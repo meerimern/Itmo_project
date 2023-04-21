805715516a5387d788b182dd4310556a282800cf
MekaErn <meerim.ernesova@gmail.com>
2023-04-21 21:29:09 +0300

    first commit

---
+++ python_training/task_8_if.py
@@ -0,0 +1,31 @@
+# num = 0
+#
+# # Также попробуйте след варианты знач num
+#
+# if num >= 0:
+#     print("Число больше либо равно 0")
+# else:
+#     print("Число отрицательное")
+
+
+
+# str_1 = "test"
+# str_2 = 'test1'
+#
+# if str_1 in str_2:
+#     print(" Содержить test ")
+# else:
+#     print("Не содержить")
+
+
+# num_float = 3.4
+#
+#
+# if num_float > 0:
+#     print("Положительное число")
+# elif num_float == 0:
+#     print("Ноль")
+# else:
+#     print("Отрицательное число")
+
+
