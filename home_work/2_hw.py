f324a8cace3447bd8aac1d5b8d00dec5434f4bb1
MekaErn <meerim.ernesova@mail.ru>
2023-04-24 11:47:23 +0300

    Homework#2

--- home_work/2_hw.py
+++ home_work/2_hw.py
@@ -1,8 +1,31 @@
-def task_1(a, b, c, x, y):
-    # a = 4
-    # b = "r"
-    # c = 1.5
-    # x = [1, 'a']
-    # y = True
+# # Task 1
+# def func():
+#     a: int = 3
+#     b: str = "Hello"
+#     c: float = 1.5
+#     x: list = [1, "j"]
+#     y: bool = False
+#     for i in(a, b, c, x, y):
+#         print(f"{i} это переменная {type(i)}")
+#
+#
+# func()
 
-print(task_1(4, 'r', 1.5, [1, 'w'], True))
+#Task 2
+#
+# def task_2()-> list:
+#     a = [1, 2, 3, 5, 8, 13, 21]
+#     print(a[0:3])
+#
+#
+# task_2()
+
+
+# #Task 3
+#
+# def task_3(n) -> int:
+#     sqr: int = n**2
+#     print(sqr)
+#
+#
+# task_3(5)
